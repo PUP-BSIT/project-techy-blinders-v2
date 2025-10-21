f840423ef2c5bfb784751fec8c06770fd7234a50
Edriane <doidoiwey@gmail.com>
2025-10-21 22:19:45 +0800

    feat: upload back-end directory

---
+++ back-end/mind_stack_id/src/main/java/com/mindstack/mind_stack_id/MindStackIdApplication.java
@@ -0,0 +1,13 @@
+package com.mindstack.mind_stack_id;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindStackIdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MindStackIdApplication.class, args);
+	}
+
+}
---
+++ back-end/mind_stack_id/src/test/java/com/mindstack/mind_stack_id/MindStackIdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mindstack.mind_stack_id;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindStackIdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
