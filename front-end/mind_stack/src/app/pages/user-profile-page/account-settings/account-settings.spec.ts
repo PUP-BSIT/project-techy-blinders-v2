3e3190c59dbf2e416c4be7efdaa8e1196d27daef
consultado-kirby <kirbyconsultado@gmail.com>
2025-11-07 16:39:47 +0800

    fix: add router testing module

--- front-end/mind_stack/src/app/pages/user-profile-page/account-settings/account-settings.spec.ts
+++ front-end/mind_stack/src/app/pages/user-profile-page/account-settings/account-settings.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AccountSettings } from './account-settings';
+import { RouterTestingModule } from '@angular/router/testing';
 
 describe('AccountSettings', () => {
   let component: AccountSettings;
@@ -8,7 +9,10 @@ describe('AccountSettings', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AccountSettings]
+      imports: [
+        AccountSettings,
+        RouterTestingModule
+      ]
     })
     .compileComponents();
 
