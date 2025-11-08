8cba5c85923071592490d11cd17e1fac71471c5d
consultado-kirby <kirbyconsultado@gmail.com>
2025-11-08 21:22:41 +0800

    feat: create edit profile component

---
+++ front-end/mind_stack/src/app/pages/user-profile-page/edit-profile-page/edit-profile-page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-profile-page',
+  imports: [],
+  templateUrl: './edit-profile-page.html',
+  styleUrl: './edit-profile-page.scss'
+})
+export class EditProfilePage {
+
+}
