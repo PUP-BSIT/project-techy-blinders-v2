c39e9d9250b5b7070a595664107193015cc80ee7
veslino-marc <veslinomarc4@gmail.com>
2025-11-12 01:16:42 +0800

    feat: add the components of community page and community service

--- front-end/mind_stack/src/app/pages/community-page/components/question-modal/question-modal/question-modal.ts
+++ front-end/mind_stack/src/app/pages/community-page/components/question-modal/question-modal/question-modal.ts
@@ -1,11 +1,44 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { CommunityService } from '../../../../../services/community.service';
 
 @Component({
   selector: 'app-question-modal',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, FormsModule],
   templateUrl: './question-modal.html',
   styleUrl: './question-modal.scss'
 })
 export class QuestionModal {
+  title: string = '';
+  content: string = '';
 
+  constructor(public communityService: CommunityService) {}
+
+  onClose() {
+    this.title = '';
+    this.content = '';
+    this.communityService.closeModal();
+  }
+
+  onSubmit() {
+    if (this.title.trim()) {
+      this.communityService.addPost({
+        title: this.title,
+        content: this.content
+      });
+      this.title = '';
+      this.content = '';
+    }
+  }
+
+  onSaveDraft() {
+    this.communityService.saveDraft({
+      title: this.title,
+      content: this.content
+    });
+    this.title = '';
+    this.content = '';
+  }
 }
