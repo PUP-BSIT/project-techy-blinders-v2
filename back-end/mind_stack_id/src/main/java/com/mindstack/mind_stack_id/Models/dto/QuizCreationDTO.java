df5ab795f6606ab1f6dd0d1023187ec214bfbd57
Edriane <doidoiwey@gmail.com>
2025-11-19 14:20:28 +0800

    feat: add data transfer object in quiz creation

--- back-end/mind_stack_id/src/main/java/com/mindstack/mind_stack_id/Models/dto/QuizCreationDTO.java
+++ back-end/mind_stack_id/src/main/java/com/mindstack/mind_stack_id/Models/dto/QuizCreationDTO.java
@@ -1,5 +1,33 @@
 package com.mindstack.mind_stack_id.Models.dto;
 
 public class QuizCreationDTO {
+    private long flashcardId;
+    private boolean identificationAnswer;
+    private String question;
+    private long quizId;
 
+    public QuizCreationDTO(){}
+
+    public QuizCreationDTO(long flashcardId, boolean identificationAnswer, String question, long quizId) {
+        this.flashcardId = flashcardId;
+        this.identificationAnswer = identificationAnswer;
+        this.question = question;
+        this.quizId = quizId;
+    }
+
+    public long getFlashcardId() {
+        return flashcardId;
+    }
+
+    public boolean getIdentifcationAnswer() {
+        return identificationAnswer;
+    }
+
+    public String getQuestion(){
+        return question;
+    }
+
+    public long getQuizId() {
+        return quizId;
+    }
 }
