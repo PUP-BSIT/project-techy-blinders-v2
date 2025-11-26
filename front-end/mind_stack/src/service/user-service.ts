98c2b0562284268b6e00d534f63216b2bc8bb7b0
Edriane <doidoiwey@gmail.com>
2025-11-26 20:40:56 +0800

    feat: add registration feature

--- front-end/mind_stack/src/service/user-service.ts
+++ front-end/mind_stack/src/service/user-service.ts
@@ -1,42 +1,19 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { HttpClientModule } from '@angular/common/http';
-
+import { RegisterRequest, RegisterResponse } from '../app/models/user.model';
 import { Observable } from 'rxjs';
-import { User,
-  RegisterRequest, 
-  RegisterResponse, 
-  LoginRequest, 
-  LoginResponse, 
-  AuthError,
-  EditProfileRequest,
-  EditProfileResponse 
- } from '../app/models/user.model';
+
 @Injectable({
   providedIn: 'root'
 })
 
 export class UserService {
-  private apiUrl = 'http://localhost:8080/api/users';
+  http = inject(HttpClient);
 
-  constructor(private http: HttpClient) {}
+  private apiUrl = 'https://techymindstack.site/api/users';
 
-  getAllUsers(): Observable <User[]> {
-    return this.http.get<User[]>(this.apiUrl)
-  }
-
-  createUser(registerRequest: RegisterRequest): Observable<RegisterResponse> {
-    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
-    return this.http.post<RegisterResponse>(this.apiUrl, registerRequest, { headers });
-  }
-
-  login(loginRequest: LoginRequest): Observable<LoginResponse> {
-    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
-    return this.http.post<LoginResponse>(`${this.apiUrl}/login`, loginRequest, { headers });
-  }
-
-  editProfile(editRequest: EditProfileRequest): Observable<EditProfileResponse> {
-    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
-    return this.http.put<EditProfileResponse>(`${this.apiUrl}/profile`, editRequest, { headers });
+  registerUser(userData: RegisterRequest): Observable<RegisterResponse> {
+    return this.http.post<RegisterResponse>(this.apiUrl, userData);
   }
 }
