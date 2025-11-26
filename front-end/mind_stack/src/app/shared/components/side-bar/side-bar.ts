6e8355207a20af0b0df003aa60dce20385564a3e
mosquito-michaelangelo <michaelmosquito147@gmail.com>
2025-11-26 21:54:52 +0800

    refactor(sidebar): update sidebar to use MatSidenav for navigation

--- front-end/mind_stack/src/app/shared/components/side-bar/side-bar.ts
+++ front-end/mind_stack/src/app/shared/components/side-bar/side-bar.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { MatSidenavModule } from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-side-bar',
   standalone: true,
-  imports: [CommonModule, RouterLink, RouterLinkActive],
+  imports: [CommonModule, RouterLink, RouterLinkActive, MatSidenavModule],
   templateUrl: './side-bar.html',
   styleUrls: ['./side-bar.scss']
 })
