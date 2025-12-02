3c7d810443e7b4f8bb6a25b7116d9b39f5067af7
Mikka Kette <esparagozamikkakette@gmail.com>
2025-12-03 00:05:44 +0800

    feat: change UI in dashboard

--- front-end/mind_stack/src/app/pages/dashboard-page/dashboard-page.ts
+++ front-end/mind_stack/src/app/pages/dashboard-page/dashboard-page.ts
@@ -29,20 +29,24 @@ export class DashboardPage {
   flashcardCount: number = 0;
   quizCount: number = 0;
   streakDays: number = 0;
+  userName: string = 'John Doe';
+  userId: string = '82739120';
 
 
   studyLibrary: StudyItem[] = [
-    { id: '1', name: 'OOP', type: 'flashcard', flashcardCount: 15 },
-    { id: '2', name: 'Web Development', type: 'flashcard', flashcardCount: 20 },
-    { id: '3', name: 'HCI', type: 'flashcard', flashcardCount: 12 },
-    { id: '4', name: 'IM', type: 'flashcard', flashcardCount: 18 }
+    { id: '1', name: 'Object Oriented Programming', type: 'flashcard', flashcardCount: 15 },
+    { id: '2', name: 'Advance Programming', type: 'flashcard', flashcardCount: 20 },
+    { id: '3', name: 'Web Development', type: 'flashcard', flashcardCount: 12 },
+    { id: '4', name: 'Project Management', type: 'flashcard', flashcardCount: 18 },
+    { id: '5', name: 'Application Development', type: 'flashcard', flashcardCount: 18 }
   ];
 
   recentActivities: Activity[] = [
-    { id: '1', type: 'flashcard', title: 'Studied Flashcards: OOP', timestamp: new Date() },
-    { id: '2', type: 'flashcard', title: 'Studied Flashcards: Web Dev', timestamp: new Date() },
-    { id: '3', type: 'quiz', title: 'Completed Quiz: OOP', timestamp: new Date() },
-    { id: '4', type: 'flashcard', title: 'Studied Flashcards: OOP', timestamp: new Date() }
+    { id: '1', type: 'flashcard', title: 'Advance Programming', timestamp: new Date() },
+    { id: '2', type: 'flashcard', title: 'Advance Programming', timestamp: new Date() },
+    { id: '3', type: 'flashcard', title: 'Advance Programming', timestamp: new Date() },
+    { id: '4', type: 'flashcard', title: 'Advance Programming', timestamp: new Date() },
+    { id: '5', type: 'flashcard', title: 'Advance Programming', timestamp: new Date() }
   ];
 
   constructor(private router: Router) {}
