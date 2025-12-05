72267ea57c4139833a513d64d2e1fb1444607878
mosquito-michaelangelo <michaelmosquito147@gmail.com>
2025-12-05 19:31:54 +0800

    feat(user-profile-page): fetch username and user ID on login

--- front-end/mind_stack/src/app/pages/user-profile-page/user-profile-page.ts
+++ front-end/mind_stack/src/app/pages/user-profile-page/user-profile-page.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { SideBar } from '../../shared/components/side-bar/side-bar';
 import { Router } from '@angular/router';
 import { EditProfilePage } from "./edit-profile-page/edit-profile-page";
 import { AccountSettings } from "./account-settings/account-settings";
+import { AuthService } from '../../../service/auth.service';
+import { LoginResponse } from '../../models/user.model';
 
 type ModalType = 'edit-profile' | 'account-settings' | null;
 
@@ -12,14 +14,19 @@ type ModalType = 'edit-profile' | 'account-settings' | null;
   templateUrl: './user-profile-page.html',
   styleUrl: './user-profile-page.scss'
 })
-export class UserProfilePage {
+export class UserProfilePage implements OnInit {
 
   showEditModal = false;
   showAccountSettingsModal = false;
   
   activeTab: 'overview' | 'account-settings' = 'overview';
+  currentUser: LoginResponse | null = null;
 
-  constructor(private route: Router) {}
+  constructor(private route: Router, private authService: AuthService) {}
+
+  ngOnInit() {
+    this.currentUser = this.authService.getCurrentUser();
+  }
   
   editProfile() {
     this.showEditModal = true;
