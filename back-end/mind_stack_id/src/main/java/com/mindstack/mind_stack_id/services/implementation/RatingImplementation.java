3dae4b7589d455d8f88d6d491ea6593dee4aadaa
Edriane <doidoiwey@gmail.com>
2025-12-26 08:44:52 +0800

    docs: remove rating feature

--- back-end/mind_stack_id/src/main/java/com/mindstack/mind_stack_id/services/implementation/RatingImplementation.java
+++
@@ -1,7 +0,0 @@
-package com.mindstack.mind_stack_id.services.implementation;
-
-import com.mindstack.mind_stack_id.services.RatingService;
-
-public class RatingImplementation implements RatingService {
-
-}
