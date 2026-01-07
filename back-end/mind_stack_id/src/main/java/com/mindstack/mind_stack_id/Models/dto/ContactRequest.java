86b1d9d34096bcef5c2014ad30acfd9b5f8e36e8
veslino-marc <veslinomarc4@gmail.com>
2026-01-07 19:53:23 +0800

    feat: add the feature for mobile in contact us also update the error message handling for minimum message

--- back-end/mind_stack_id/src/main/java/com/mindstack/mind_stack_id/Models/dto/ContactRequest.java
+++ back-end/mind_stack_id/src/main/java/com/mindstack/mind_stack_id/Models/dto/ContactRequest.java
@@ -5,17 +5,16 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public class ContactRequest {
-    
+
     @NotBlank(message = "Name is required")
     @Size(min = 2, max = 100, message = "Name must be between 2 and 100 characters")
     private String name;
-    
+
     @NotBlank(message = "Email is required")
     @Email(message = "Invalid email format")
     private String email;
-    
+
     @NotBlank(message = "Message is required")
-    @Size(min = 10, max = 1000, message = "Message must be between 10 and 1000 characters")
     private String message;
 
     public String getName() {
